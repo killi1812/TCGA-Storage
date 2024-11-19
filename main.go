b6c74d1832505ae9a6a9f23b411bd071a1f3aa33
killi1812 <cvok.fran@gmail.com>
2024-11-19 20:57:18 +0100

    serving wwwroot

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	print("Bokic")
+	fmt.Println("App started")
+	http.Handle("/", http.FileServer(http.Dir("./wwwroot/")))
+	fmt.Println("listeing in http://localhost:3050")
+	err := http.ListenAndServe(":3050", nil)
+	if err != nil {
+		print(err)
+	}
 }
